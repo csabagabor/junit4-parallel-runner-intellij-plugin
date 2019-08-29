e9f39bd9fb727476bb8132e6055ac5548d52e9d6
csaba97 <37183688+csaba97@users.noreply.github.com>
2019-08-29 09:12:15 +0300

    Update ParallelSuite.java

--- src/com/googlecode/junittoolbox/ParallelSuite.java
+++ src/com/googlecode/junittoolbox/ParallelSuite.java
@@ -1,15 +1,53 @@
 package com.googlecode.junittoolbox;
 
+import com.intellij.openapi.util.io.FileUtilRt;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ParallelSuite extends Suite {
 
     public ParallelSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError
             , ClassNotFoundException {
-        super(builder, klass, new Class<?>[]{Class.forName("TestTest"), Class.forName("TestTest2")});
+        super(builder, klass, getClasses());
         setScheduler(new ParallelScheduler());
+    }
+
+    private static Class<?>[] getClasses() {
+        List<Class<?>> classes = new ArrayList<>();
+        //com.intellij.junit4.JUnit4IdeaTestRunner
+        File dir = new File(FileUtilRt.getTempDirectory());
+
+        File file = new File(dir, "jun_par_tes.tmp");
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file.getName()));
+
+            String className = reader.readLine();
+            if (className != null) {
+                classes.add(Class.forName(className));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return (Class<?>[]) classes.toArray();
     }
 }
