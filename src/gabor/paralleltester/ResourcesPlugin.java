6f31f709414f077f73f35af1e446d9aeeb5aa968
csaba97 <37183688+csaba97@users.noreply.github.com>
2019-08-31 20:13:17 +0300

    Update ResourcesPlugin.java

--- src/gabor/paralleltester/ResourcesPlugin.java
+++ src/gabor/paralleltester/ResourcesPlugin.java
@@ -5,13 +5,12 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public abstract class ResourcesPlugin {
-    public static String s = "run16.svg";
 
-    public static final Icon RUN_13 = IconLoader.getIcon(s);
+    public static final Icon RUN_13 = IconLoader.getIcon("run16.svg");
 
-    public static final Icon RUN = IconLoader.getIcon(s);
+    public static final Icon RUN = IconLoader.getIcon("run16.svg");
 
-    public static final Icon DEBUG_13 = IconLoader.getIcon(s);
+    public static final Icon DEBUG_13 = IconLoader.getIcon("debug16.svg");
 
-    public static final Icon DEBUG = IconLoader.getIcon(s);
+    public static final Icon DEBUG = IconLoader.getIcon("debug16.svg");
 }
