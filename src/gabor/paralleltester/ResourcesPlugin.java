1f63cef296a5e6e125335f5c47907bf3502bb6a6
csaba97 <37183688+csaba97@users.noreply.github.com>
2019-09-01 12:19:20 +0300

    change folder

--- src/gabor/paralleltester/ResourcesPlugin.java
+++ src/gabor/paralleltester/ResourcesPlugin.java
@@ -6,11 +6,11 @@ import javax.swing.*;
 
 public abstract class ResourcesPlugin {
 
-    public static final Icon RUN_13 = IconLoader.getIcon("run16.svg");
+    public static final Icon RUN_13 = IconLoader.getIcon("/icons/run16.svg");
 
-    public static final Icon RUN = IconLoader.getIcon("run16.svg");
+    public static final Icon RUN = IconLoader.getIcon("/icons/run16.svg");
 
-    public static final Icon DEBUG_13 = IconLoader.getIcon("debug16.svg");
+    public static final Icon DEBUG_13 = IconLoader.getIcon("/icons/debug16.svg");
 
-    public static final Icon DEBUG = IconLoader.getIcon("debug16.svg");
+    public static final Icon DEBUG = IconLoader.getIcon("/icons/debug16.svg");
 }
