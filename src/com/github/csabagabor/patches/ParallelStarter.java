499552156ecd9d4dc5960bb52b78c6d70c60a83a
csabagabor <37183688+csaba97@users.noreply.github.com>
2019-09-28 09:24:39 +0300

    small change

--- src/com/github/csabagabor/patches/ParallelStarter.java
+++ src/com/github/csabagabor/patches/ParallelStarter.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package com.github.csabagabor.patches;
 
 import com.github.csabagabor.Resources;
